a0d1a455e1eae1c7df8bd4ed272affd680b482d0
JudeSharo <arthurdelhayepro@gmail.com>
2021-04-10 09:26:32 +0200

    .en.local modifié, bdd event créé, début du front

--- migrations/Version20210409153859.php
+++ migrations/Version20210409153859.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210409095650 extends AbstractMigration
+final class Version20210409153859 extends AbstractMigration
 {
     public function getDescription() : string
     {
