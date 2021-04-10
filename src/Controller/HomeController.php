08d807c007d0e7b6f028f9073951afa19add799d
JudeSharo <arthurdelhayepro@gmail.com>
2021-04-10 20:01:16 +0200

    Ajout de la création d'évent+ affichage de tous les évents dans la page d'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,37 +1,47 @@
 <?php
 
 namespace App\Controller;
-
 use App\Entity\Event;
+use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-	private $em;
-	public function __construct(EntityManagerInterface $em)
-	{
-		$this->em=$em;
-	}
     #[Route('/', name: 'home')]
-    public function index():Response
+    public function index(EventRepository $repo):Response
     {
     	
-    	$date = new \DateTime('@'.strtotime('now'));
-    	$event = new Event;
-    	$event ->setTitle('Le deuxieme cadeau');
-    	$event ->setDescription("En effet, le grand roi a tous fais");
-    	$event ->setDateEvent($date);
-   		$event ->setImage("chemin de l'image");
-   		//dd($event);
-
-   		$this ->em ->persist($event);
-   		$this ->em ->flush();
-
-        return $this->render("home/home.html.twig");
+        return $this->render("home/home.html.twig",['event'=>$repo->findAll()]);
     }
+    #[Route('/event/create', name: 'create')]
+    public function create(Request $request,EntityManagerInterface $em)
+    {
+    	if($request->isMethod('POST'))
+    	{
+    		$data = $request ->request -> all();
+    		if($this->isCsrfTokenValid('event_key',$data['_token']))
+    		{
+    			$event = new Event;
+
+    			$event->setTitle($data['title']);
+    			$event->setDescription($data['description']);
+    			$event->setDateEvent(new \DateTime());
+    			$event->setImage("veqp");
+
+    			$em->persist($event);
+    			$em->flush(); 
+    		}
+    		return $this->redirectToRoute("home");
+
+    	}
+    	return $this->render("home/create.html.twig");
+    }
+
 }
 
 
